3906eb64f8f682ad85e53443a5f48d1f7412fc65
Junki Yonezu <yonegen0@gmail.com>
2025-06-22 22:32:40 +0900

    設定修正

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,8 @@ import type { StorybookConfig } from "@storybook/nextjs-vite";
 const config: StorybookConfig = {
   "stories": [
     "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../src/**/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   "addons": [
     "@chromatic-com/storybook",
