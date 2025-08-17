ac735bdc831bcd44c570ade517efa7e1a3a7a578
Junki Yonezu <yonegen0@gmail.com>
2025-08-17 20:29:42 +0900

    一部修正

--- src/stories/organisms/RoomListMenu.stories.tsx
+++ src/stories/organisms/RoomListMenu.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/nextjs-vite';
 import { fn } from 'storybook/test';
 import { RoomListMenu } from '../../components/organisms/RoomListMenu';
-import { useState } from 'react';
 
 const meta = {
   title: 'Example/RoomListMenu',
@@ -13,27 +12,20 @@ const meta = {
 } satisfies Meta<typeof RoomListMenu>;
 
 export default meta;
+
 type Story = StoryObj<typeof meta>;
 
-// useStateをラップするコンポーネント
-const WrapperComponent = () => {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-
-  return (
-    <RoomListMenu
-      open={true}
-      anchorEl={anchorEl}
-      onClose={fn()}
-      rooms={['aaa', 'bbb']}
-      currentRoom="aaa"
-      onRoomChange={fn()}
-      onOpenDeleteConfirmDialog={fn()}
-      showAddRoomButton={true}
-      onOpenCreateRoomDialog={fn()}
-    />
-  );
-};
-
 export const Default: Story = {
-  render: () => <WrapperComponent />,
+  // ここにargsを追加する
+  args: {
+    open: true,
+    anchorEl: null, // 初期値はnull
+    rooms: ['aaa', 'bbb'],
+    currentRoom: 'aaa',
+    onClose: fn(),
+    onRoomChange: fn(),
+    onOpenDeleteConfirmDialog: fn(),
+    showAddRoomButton: true,
+    onOpenCreateRoomDialog: fn(),
+  },
 };
